56982844b7396e0a50748f0f548e3f77958d2945
Black python <black_python@elude.in>
2019-12-03 10:25:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+import tqdm
+
+denera = 1_954_537
+
+bautizo_file = open("bautizo.csv", "a")
+b_writter = csv.writer(bautizo_file, delimiter='|', quoting=csv.QUOTE_MINIMAL)
+hileta_file = open("hileta.csv", "a")
+d_writter = csv.writer(hileta_file, delimiter='|', quoting=csv.QUOTE_MINIMAL)
+ezkontza_file = open("ezkontza.csv", "a")
+m_writter = csv.writer(ezkontza_file, delimiter='|', quoting=csv.QUOTE_MINIMAL)
+
+values = {"b": b_writter, "d": d_writter, "m": m_writter}
+
+for id in tqdm.tqdm(range(1, denera + 1)):
+    orrialdea = f"https://artxiboa.mendezmende.org/es/busque-partidas-sacramentales/ver.html?id={id}&sacramento="
+    for k, v in values.items():
+        page = requests.get(url=orrialdea + k)
+        if page.status_code != 404:
+            fitxategia = v
+            break
+
+    content = page.content
+    soup = BeautifulSoup(content, 'html.parser')
+
+    taula = soup.find('section', id='identificacion')
+
+    ident = [td.get_text() for td in taula.find_all('td')]
+
+    taula2 = soup.find('section', id='localizacion')
+    loc = [td.get_text() for td in taula2.find_all('td')]
+
+    fitxategia.writerow([id] + ident + loc)
